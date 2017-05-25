38f97a686caab0021e1b5bc37d3e4bb3e792c2ea
David Stephens <pushingrobot@me.com>
2017-05-25 16:54:23 -0600

    add to package.json

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,9 @@
+var path = require('path');
+
 module.exports = {
   entry: './app/index.js',
   output: {
-    filename: 'dist/bundle.js'
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
   }
 }
