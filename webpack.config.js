6a3c81b0dd9e714ebe5bdccaab0f1046d00d42e7
David Stephens <pushingrobot@me.com>
2017-05-25 22:20:51 -0600

    Split CSS out with ExtractTextPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,21 @@
-var path = require('path');
+const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
   entry: ['./app/index.js', './vendor.js'],
   output: {
     filename: '[name].js',
-    path: path.resolve(__dirname, 'dist')
+    path: path.resolve(__dirname, 'dist'),
   },
   module: {
     rules: [{
       test: /\.css$/,
-      use: ['style-loader', 'css-loader']
-    }]
-  }
+      use: ExtractTextPlugin.extract({
+        use: 'css-loader',
+      }),
+    }],
+  },
+  plugins: [
+    new ExtractTextPlugin('styles.css'),
+  ],
 }
